fdc0e61fd5e253c75eee3b9429e856ebff045e28
chandshaik99 <105013138+chandshaik99@users.noreply.github.com>
2022-08-24 21:09:08 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to NEW WORLD" }');
+    res.send('{ "response": "Hello, Welcome to WORLD" }');
 });
 
 app.get('/will', function (req, res) {
